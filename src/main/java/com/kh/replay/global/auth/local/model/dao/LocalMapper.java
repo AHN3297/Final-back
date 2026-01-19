0c0ac86fa0040ec03e642211dac94d4e6f8e71f9
PRJuyoungBae <juyoungbae02@gmail.com>
2026-01-19 11:03:05 +0900

    feat: 회원가입 기능 구현

--- src/main/java/com/kh/replay/global/auth/local/model/dao/LocalMapper.java
+++ src/main/java/com/kh/replay/global/auth/local/model/dao/LocalMapper.java
@@ -1,13 +1,13 @@
 package com.kh.replay.global.auth.local.model.dao;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
+import com.kh.replay.global.auth.local.model.dto.LocalDTO;
 import com.kh.replay.global.member.model.vo.MemberVO;
 @Mapper
 public interface LocalMapper {
-	@Insert("INSERT INTO TB_LOCAL (MEMBERID,PASSWORD) VALUES (#{memberId},#{password})")
-
-	 int signUp(MemberVO member);		
+	
+	int insertMember(MemberVO member);		
+	int signUp(LocalDTO localDto);
 		
 		
 
--- src/main/java/com/kh/replay/global/auth/local/model/dto/LocalDTO.java
+++ src/main/java/com/kh/replay/global/auth/local/model/dto/LocalDTO.java
@@ -14,9 +14,14 @@ public class LocalDTO {
 	private MemberDTO memberDto;
 	
 	@NotBlank(message="비밀번호를 입력해주세요.")
-	@Pattern(regexp="^(?=.*[A-Za-z])(?=.*\\\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$",message="비밀번호는 8자 이상, 영문/숫자/특수문자를 포함해야 합니다.")
+	//@Pattern(regexp="^(?=.*[A-Za-z])(?=.*\\\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$",message="비밀번호는 8자 이상, 영문/숫자/특수문자를 포함해야 합니다.")
 	private String password;
 	
+	@NotBlank(message="memberId는 필수입니다.")
+	@Pattern(regexp="^#.*", message = "memberId는 #으로 시작해야 합니다")
+	private String memberId;
+	//인코딩된 비밀번호
+
 	
 
 }

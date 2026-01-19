6ff6b21d4e0904393c7d5054d4f7a031ba4a615b
wookidoki <wookidoki98@gmail.com>
2026-01-19 15:40:12 +0900

    fix: 파일경로최신화 , 과거 코드 수정, 유니버스전체목록조회

--- src/main/java/com/kh/replay/global/auth/local/model/service/LocalServiceImpl.java
+++ src/main/java/com/kh/replay/global/auth/local/model/service/LocalServiceImpl.java
@@ -1,7 +1,10 @@
 package com.kh.replay.global.auth.local.model.service;
 
+import org.springframework.stereotype.Service;
+
 import com.kh.replay.global.auth.local.model.dto.LocalDTO;
 
+@Service
 public class LocalServiceImpl implements LocalService {
 
 	@Override

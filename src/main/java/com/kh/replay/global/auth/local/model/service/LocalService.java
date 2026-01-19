eaeed2c676acf9ec44dd8374975c5863b2c5cf87
kanghyunseong <gustjd2754@daum.net>
2026-01-19 15:40:12 +0900

    fix: 파일경로최신화 , 과거 코드 수정, 유니버스전체목록조회

--- src/main/java/com/kh/replay/global/auth/local/model/service/LocalService.java
+++ src/main/java/com/kh/replay/global/auth/local/model/service/LocalService.java
@@ -1,7 +1,8 @@
 package com.kh.replay.global.auth.local.model.service;
 
-import com.kh.replay.global.auth.local.model.dto.LocalDTO;
+import org.springframework.stereotype.Service;
 
+import com.kh.replay.global.auth.local.model.dto.LocalDTO;
 
 
 public interface LocalService {

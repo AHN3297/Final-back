2669f29522bfcf5f99456089fc131f6ae9500be4
AHN3297 <ga33229977@gmail.com>
2026-01-23 17:02:11 +0900

    feat : 중간저장

--- src/main/java/com/kh/replay/global/like/LikeController/LikeController.java
+++ src/main/java/com/kh/replay/global/like/LikeController/LikeController.java
@@ -3,6 +3,7 @@ package com.kh.replay.global.like.LikeController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -36,6 +37,10 @@ public class LikeController {
     }
 
     // 좋아하는 가수 조회
+    @GetMapping("/artists")
+    public ResponseEntity<ResponseData<LikeResponse>> findAllLikeArtist(){
+    	
+    }
     
     
     // 좋아하는 가수 삭제

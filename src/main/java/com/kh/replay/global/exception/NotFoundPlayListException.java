7c2a86c693da06bd83574d4273542ffb7ea3f476
AHN3297 <ga33229977@gmail.com>
2026-01-23 10:38:48 +0900

    Feat: PlayListTracks

--- src/main/java/com/kh/replay/global/exception/NotFoundPlayListException.java
+++
@@ -1,7 +0,0 @@
-package com.kh.replay.global.exception;
-
-public class NotFoundPlayListException extends RuntimeException{
-	public NotFoundPlayListException(String message) {
-		super(message);
-	}
-}
